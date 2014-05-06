d7d47c03df5d6b3bd40720e881d4305d93115f94
Acaldas <afonso7caldas@gmail.com>
2014-05-06 22:29:10 +0100

    merge

--- public/movies/controllers/movieControllers.js
+++ public/movies/controllers/movieControllers.js
@@ -9,13 +9,19 @@ angular.module('mean.movies').controller('MoviesMainController', ['$scope', '$st
         $scope.movies = response.movie;
       });
       
-
+      $scope.setMovie = function(movie){
+            //rottentomatoesService.setMovie(movie);
+            $location.path('/movies/' + movie.id);
+      };
       // Get movie with id 1
       //$scope.test = Movies.getMovies.get({},{'id': 1});      
       //http://www.masnun.com/2013/08/28/rest-access-in-angularjs-using-ngresource.html
 
       //$scope.update = Movies.update.query();
       //console.log($scope.update);
-      //$scope.movies = $scope.update.movies;
   }
-]);
+])
+.controller('MovieController', ['$scope', '$stateParams', Movies, function($scope, $stateParams, Movies) {
+
+
+}]);
